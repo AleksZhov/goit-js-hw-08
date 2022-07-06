2f642dfd566d399ebd45258c59fa5d0eaf63b93d
AleksZhov <alekszhov@gmail.com>
2022-07-06 10:50:46 +0300

    added all files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const transformResult = galleryItemTransform(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', transformResult);
+
+let lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function galleryItemTransform(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+const onPlay = function (data) {
+  let currentTime = player.getCurrentTime().then(function (seconds) {
+    // seconds = the current playback position
+  });
+  return currentTime;
+};
+
+player.on('timeupdate', onPlay);
+player.off('timeupdate', offPlay);
+
+let currentTime = player
+  .getCurrentTime()
+  .then(function (seconds) {
+    // seconds = the current playback position
+  })
+  .catch(function (error) {
+    // an error occurred
+  });
+console.log(currentTime);
+console.log(localStorage);
+localStorage.setItem('videoplayer-current-time', currentTime);
